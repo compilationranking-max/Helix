8a550ede29b0667de846315c8f816eab794f8c1c
compilationranking-max <compilationranking@gmail.com>
2026-09-20 12:37:51 +0530

    Use same-origin Helix API

--- app.js
+++ app.js
@@ -398,7 +398,7 @@ async function sendAIMessage() {
     setAIProcessing(true);
 
     try {
-        const response = await fetch("http://localhost:3000/api/chat", {
+        const response = await fetch("/api/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
